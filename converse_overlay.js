870e4411c49626c6682235cf98e028206c3ce79e
Dennis Liaw <dennis.liaw@gmail.com>
2015-03-31 04:13:50 -0400

    adds item check

---
+++ converse_overlay.js
@@ -0,0 +1,9 @@
+
+//Check to see if we have more than 2 items
+var twoOrMoreItems = function(){
+  var cartCount = parseInt ($("#mini-cart").attr("icon-text-attr") );
+  return cartCount >= 2;
+}
+//pull elements from page
+
+//render overlay

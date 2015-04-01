b893024f740a5b0b6d2a3bef7df074d0ed6e3f3b
Dennis Liaw <dennis.liaw@gmail.com>
2015-04-01 09:56:00 -0400

    COMMENCE STYLING

--- README.md
+++ README.md
@@ -1,22 +1,29 @@
 # Converse Ad Overlay
 
-Hello Bouncee Exchange! This is a solution to the code challenge Angela sent me. Below is the prompt for the problem: 
+Hello Bouncee Exchange! This is a solution to the code challenge Angela sent me. Below is the prompt for the problem:
 
 > Go to converse.com and add at least 2 products to your cart. Then return to the home page.
-> 
+>
 > Write a javascript snippet that can be run in the console of a browser that does the following:
-> 
-> Extract the number of items in the cart, the cart total, and item images from the page. Store them in javascript variables. 
-> 
+>
+> Extract the number of items in the cart, the cart total, and item images from the page. Store them in javascript variables.
+>
 > The trigger should show a centered overlay on top of the site that displays the information gathered above and two buttons. One button closes the overlay and the other takes the user to the cart page. It should have a style consistent with the website. Design matters.
-> 
+>
 > Behind the overlay add a semi-transparent black background that obscures the site. The overlay should be able to trigger multiple times if dismissed.
-> 
+>
 > BONUS
-> 
+>
 > 1. The overlay should only fire if the user is "logged in".
-> 
+>
 > 2. Describe some other ways you might be able to obtain the information needed for the overlay (think generally, not specific to THIS website).
+
+
+# To Run
+
+Copy and paste the code into your browser's javascript console, then run the command:
+
+generateOverlay()
 
 # The Apporach
 
--- converse_overlay.js
+++ converse_overlay.js
@@ -1,44 +1,70 @@
 
-var twoOrMoreItems = function(){
-  var cartCount = parseInt ($("#mini-cart").attr("icon-text-attr") );
-  return cartCount >= 2;
-};
+  var twoOrMoreItems = function(){
+    var cartCount = parseInt ($("#mini-cart").attr("icon-text-attr") );
+    return cartCount >= 2;
+  };
 
-var getImgTags = function($cart){
-  return $cart.find( ".item-image img");
-};
+  var getImgTags = function($cart){
+    return $cart.find( ".item-image img"); //can change the search param as needed if client changes layout
+  };
 
-var styleOverlay = function($div){
+  var styleOverlay = function($div){
+    $div.css({ 'background-color': 'rgba(0,0,0,0.5)',
+                       'position': 'fixed',
+                        'z-index': '1000',
+                          'width': '100%',
+                         'height': '100%'
+              });
+  };
 
-};
+  var styleContent = function($content){
+    $content.css({ 'background-color': 'white',
+                              'width': '600px',
+                              'color': 'black',
+                             'margin': '0 auto',
+                           'position': 'relative',
+                                'top': '50%',
+                          'transform': 'translateY(-50%)',
+                         'text-align': 'center',
+                      'border-radius': '5px'
+                  });
+  };
 
 
-var populateDiv = function($div){
-  $.ajax( {      url: "/cart",
-                type: "get",
-            dataType: "html"
-          }
-       ).success( function(data){
-          var images = getImgTags( $(data) );
-          var price  = $("<p>");
-          price.text( "$" + cy.VALUE );//modify for different currencies. cy is a var in hompage
-          $div.append( images );
-          $div.append( price );
-          styleOverlay($div);
-          $('body').append( $div );
+  var populateDiv = function($overlay){
+    $.ajax( {      url: "/cart",
+                  type: "get",
+              dataType: "html"
+            }
+         ).success( function(data){
+            var content = $("<div>");
+            var images = getImgTags( $(data) );
+            var price  = $("<p>");
 
-          console.log("SUCCESS!");
-       }).fail( function(error){
-          console.log("ERROR:\n" + error);
-       });
-};
+            price.text( "$" + cy.VALUE );// modify for different currencies. cy is a var in hompage
+            content.attr( "id", "bouncex"); // for debugging
 
-var generateOverlay= function(){
-  if( twoOrMoreItems() ){
-    var overlay = $("<div>");
-    overlay.addClass("bouncy");
-    populateDiv(overlay); //AJAX the cart page and populate the div
-  } else {
-    alert("Not enough items to trigger overlay!");
-  }
-};
+            content.append( images );
+            content.append( price );
+            $overlay.append( content);
+
+            styleOverlay( $overlay );
+            styleContent( content );
+
+            $('body').append( $overlay );
+
+            console.log("SUCCESS!");
+         }).fail( function(error){
+            console.log("ERROR:\n" + error);
+         });
+  };
+
+  var generateOverlay= function(){
+    if( twoOrMoreItems() ){
+      var overlay = $("<div>");
+      overlay.attr( "id", "overlay"); // debug purposes
+      populateDiv(overlay); //AJAX the cart page and populate the div
+    } else {
+      alert("Not enough items to trigger overlay!");
+    }
+  };

ac83a6d6865b5e673b4df1e1efb4e9a654cc9d01
Dennis Liaw <dennis.liaw@gmail.com>
2015-04-01 10:48:37 -0400

    adds buttons to content

--- converse_overlay.js
+++ converse_overlay.js
@@ -1,88 +1,106 @@
 
-  var twoOrMoreItems = function(){
-    var cartCount = parseInt ($("#mini-cart").attr("icon-text-attr") );
-    return cartCount >= 2;
-  };
+var twoOrMoreItems = function(){
+  var cartCount = parseInt ($("#mini-cart").attr("icon-text-attr") );
+  return cartCount >= 2;
+};
 
-  var getImgTags = function($cart){
-    return $cart.find( ".item-image img"); //can change the search param as needed if client changes layout
-  };
+var getImgTags = function($cart){
+  return $cart.find( ".item-image img"); //can change the search param as needed if client changes layout
+};
 
-  var styleOverlay = function($div){
-    $div.css({ 'background-color': 'rgba(0,0,0,0.75)',
-                       'position': 'fixed',
-                        'z-index': '10000', //header has a z-index of 9999...
-                          'width': '100%',
-                         'height': '100%'
-              });
-  };
+var createButtons = function($content){
 
-  var styleContent = function($content){
-    $content.css({ 'background-color': 'white',
-                              'width': '600px',
-                              'color': 'black',
-                             'margin': '0 auto',
-                           'position': 'relative',
-                                'top': '50%',
-                          'transform': 'translateY(-50%)',
-                         'text-align': 'center',
-                      'border-radius': '5px'
-                  });
-  };
+  var closeBTN = $("<div>");
+  var cartBTN = $("<a>");
 
-  var styleButtons = function(){
-        $("bounceEX-BTN").css({  "width":"25%",
-                                 "height":"50%"
+  closeBTN.attr("id", "bouncexClose")
+  closeBTN.text("Close");
+
+  cartBTN.html('<div id="bouncexCart">Cart</div>');
+  cartBTN.attr("href", "/cart");
+
+  closeBTN.addClass("bounceEX-BTN");
+  cartBTN.addClass( "bounceEX-BTN");
+
+  styleButtons();
+
+  $content.append( closeBTN );
+  $content.append( cartBTN );
+};
+
+var styleOverlay = function($div){
+  $div.css({ 'background-color': 'rgba(0,0,0,0.75)',
+                     'position': 'fixed',
+                      'z-index': '10000', //header has a z-index of 9999...
+                        'width': '100%',
+                       'height': '100%'
+            });
+};
+
+var styleContent = function($content){
+  $content.css({ 'background-color': 'white',
+                            'width': '600px',
+                            'color': 'black',
+                           'margin': '0 auto',
+                         'position': 'relative',
+                              'top': '50%',
+                        'transform': 'translateY(-50%)',
+                       'text-align': 'center',
+                    'border-radius': '5px'
+                });
+};
+
+var styleButtons = function(){
+  $(".bounceEX-BTN").css({  "width":"25%",
+                            "height":"50%"
 
 
 
-                              });
-  };
+                        });
+
+  $("#bouncexClose").css({"background-color":"red"});
+  $("#bouncexCart").css({"background-color":"green"});
+
+};
 
 
-  var populateDiv = function($overlay){
-    $.ajax( {      url: "/cart",
-                  type: "get",
-              dataType: "html"
-            }
-         ).success( function(data){
-            var content = $("<div>");
-            var images = getImgTags( $(data) );
-            var price  = $("<p>");
-            var closeBTN = $("<div>");
-            var cartBTN = $("<a>");
+var populateDiv = function($overlay){
+  $.ajax( {      url: "/cart",
+                type: "get",
+            dataType: "html"
+          }
+       ).success( function(data){
+          var content = $("<div>");
+          var images = getImgTags( $(data) );
+          var price  = $("<p>");
 
-            closeBTN.attr("id", "bouncexClose")
-            cartBTN.html('<div id="bouncexCart"></div>');
+          price.text( "$" + cy.VALUE );// modify for different currencies. cy is a var in hompage
+          content.attr( "id", "bouncex"); // for debugging
 
-            closeBTN.addClass("bounceEX-BTN");
-            cartBTN.addClass( "bounceEX-BTN");
+          content.append( images );
+          content.append( price );
 
-            price.text( "$" + cy.VALUE );// modify for different currencies. cy is a var in hompage
-            content.attr( "id", "bouncex"); // for debugging
+          $overlay.append( content);
 
-            content.append( images );
-            content.append( price );
-            $overlay.append( content);
+          styleOverlay( $overlay );
+          styleContent( content );
 
-            styleButtons();
-            styleOverlay( $overlay );
-            styleContent( content );
+          createButtons( content );
 
-            $('body').append( $overlay );
+          $('body').append( $overlay );
 
-            console.log("SUCCESS!");
-         }).fail( function(error){
-            console.log("ERROR:\n" + error);
-         });
-  };
+          console.log("SUCCESS!");
+       }).fail( function(error){
+          console.log("ERROR:\n" + error);
+       });
+};
 
-  var generateOverlay= function(){
-    if( twoOrMoreItems() ){
-      var overlay = $("<div>");
-      overlay.attr( "id", "overlay"); // debug purposes
-      populateDiv(overlay); //AJAX the cart page and populate the div
-    } else {
-      alert("Not enough items to trigger overlay!");
-    }
-  };
+var generateOverlay= function(){
+  if( twoOrMoreItems() ){
+    var overlay = $("<div>");
+    overlay.attr( "id", "overlay"); // debug purposes
+    populateDiv(overlay); //AJAX the cart page and populate the div
+  } else {
+    alert("Not enough items to trigger overlay!");
+  }
+};
